8e451c565691689bdd6bded29ab609820dfbbd33
Chae-woo "Mac" Yoo <44356908+Turbstructor@users.noreply.github.com>
2024-04-19 23:05:12 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 112 ms, Memory: 13212 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main()
+{
+    var reader: Scanner = Scanner(System.`in`)
+    var n: Int = reader.nextInt()
+    var k: Int = reader.nextInt()
+    var a: Int = 0
+    var b: Int = 0
+    
+    for(i in 1..n)
+    {
+        a = reader.nextInt()
+        b = reader.nextInt()
+    }
+    
+    println("비와이")
+}
